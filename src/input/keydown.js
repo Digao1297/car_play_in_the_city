b5cfb20483e99fac44db3475e43e212c2a308e46
digao1297 <digao1297@gmail.com>
2021-04-22 23:26:36 -0300

    adicionado audio

--- src/input/keydown.js
+++ src/input/keydown.js
@@ -1,8 +1,25 @@
 export default function keydown(e) {
-    if (keysActions[e.code]) {
-        actions[keysActions[e.code]] = true;
-        e.preventDefault();
-        e.stopPropagation();
-        return false;
-    }
+  switch (keysActions[e.code]) {
+    case "acceleration":
+      soundCarRun.setVolume(0.1);
+      if (!soundCarRun.isPlaying) {
+        soundCarRun.play();
+        soundCarStop.pause();
+      }
+      break;
+    case "braking":
+      soundCarStop.setVolume(0.3);
+      if (!soundCarStop.isPlaying) {
+        soundCarStop.play();
+        soundCarRun.pause();
+      }
+      break;
+  }
+
+  if (keysActions[e.code]) {
+    actions[keysActions[e.code]] = true;
+    e.preventDefault();
+    e.stopPropagation();
+    return false;
+  }
 }
--- src/input/keyup.js
+++ src/input/keyup.js
@@ -1,8 +1,27 @@
 export default function keyup(e) {
-    if (keysActions[e.code]) {
-        actions[keysActions[e.code]] = false;
-        e.preventDefault();
-        e.stopPropagation();
-        return false;
-    }
+  switch (keysActions[e.code]) {
+    case "acceleration":
+      soundCarRun.setVolume(0.1);
+      if (!soundCarRun.isPlaying) {
+        soundCarRun.play();
+        soundCarStop.pause();
+      }
+      break;
+    case "braking":
+      soundCarStop.setVolume(0.3);
+      if (!soundCarStop.isPlaying) {
+        soundCarStop.play();
+        soundCarRun.pause();
+      }
+      break;
+    default:
+      break;
+  }
+
+  if (keysActions[e.code]) {
+    actions[keysActions[e.code]] = false;
+    e.preventDefault();
+    e.stopPropagation();
+    return false;
+  }
 }
